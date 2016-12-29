d7329c8be0e7c3dfe19896ce25560bffb98552d2
isitLoVe <isitl0v3@gmail.com>
2016-12-29 14:32:17 +0100

    De Profundis

--- AtlasLoot/Addons/classpriority.lua
+++ AtlasLoot/Addons/classpriority.lua
@@ -1,4 +1,118 @@
 AtlasLootClassPriority = {
+	-----------------
+	--- Naxxramas ---
+	-----------------
+
+Item23069 = "YES"; --Necro-knight Garb
+Item22935 = "YES"; --Touch of Frost
+Item22940 = "YES"; --Icebane Pauldrons
+Item22941 = "YES"; --Polar Shoulderpads
+Item22968 = "YES"; --Glacial Mantle
+Item22967 = "YES"; --Icy Scale Spaulders
+Item22810 = "YES"; --Toxin Injector
+Item23028 = "YES"; --Hailstone Band
+Item23019 = "YES"; --Icebane Helmet
+Item23033 = "YES"; --Icy Scale Coif
+Item23042 = "YES"; --Loatheb's Reflection
+Item23073 = "YES"; --Boots of Displacement
+Item23032 = "YES"; --Glacial Headdress
+Item23020 = "YES"; --Polar Helmet
+Item22809 = "YES"; --Maul of the Redeemed Crusader
+
+	---------------------------
+	--- Temple of Ahn'Qiraj ---
+	---------------------------
+	
+Item21838 = "YES"; --Garb of Royal Ascension
+Item21888 = "YES"; --Gloves of the Immortal
+Item21856 = "YES"; --Neretzek, The Blood Drinker
+Item21891 = "YES"; --Shard of the Fallen Star
+Item21702 = "YES"; --Amulet of Foul Warding
+Item21708 = "YES"; --Beetle Scaled Wristguards
+Item21697 = "YES"; --Cape of the Trinity
+Item21696 = "YES"; --Robes of the Triumvirate
+Item21691 = "YES"; --Ooze-ridden Gauntlets
+Item21685 = "YES"; --Petrified Scarab
+Item21682 = "YES"; --Bile-Covered Gauntlets
+Item21684 = "YES"; --Mantle of the Desert's Fury
+Item21687 = "YES"; --Ukko's Ring of Darkness
+Item21678 = "YES"; --Necklace of Purity
+Item21648 = "YES"; --Recomposed Boots
+Item21627 = "YES"; --Cloak of Untold Secrets
+Item21647 = "YES"; --Fetish of the Sand Reaver
+Item21652 = "YES"; --Silithid Carapace Chestguard
+Item21624 = "YES"; --Gauntlets of Kalimdor
+Item21626 = "YES"; --Slime-coated Leggings
+Item21607 = "YES"; --Grasp of the Fallen Emperor
+Item21679 = "YES"; --Kalimdor's Revenge
+Item23558 = "YES"; --The Burrower's Shell
+Item21579 = "YES"; --Vanquished Tentacle of C'thun
+Item21221 = "YES"; --Eye of C'thun
+
+	----------------------
+	--- Blackwing Lair ---
+	----------------------
+
+Item19354 = "YES"; --Draconic Avenger
+Item19358 = "YES"; --Draconic Maul
+Item19439 = "YES"; --Interlaced Shadow Jerkin
+Item19334 = "YES"; --The Untamed Blade
+Item20383 = "YES"; --Head of Broodlord Lashlayer
+Item19353 = "YES"; --Drake Talon Cleaver
+Item19344 = "YES"; --Natural Alignment Crystal
+Item19401 = "YES"; --Primalist's Linked Legguards
+Item19433 = "YES"; --Emberweave Leggings
+Item19357 = "YES"; --Herald of Woe
+Item19393 = "YES"; --Primalist's Linked Waistguard
+Item19378 = "YES"; --Cloak of the Brood Lord
+Item19003 = "YES"; --Head of Nefarian
+Item19383 = "YES"; --Head of Nefarian
+Item19366 = "YES"; --Head of Nefarian
+Item19384 = "YES"; --Head of Nefarian
+
+	-------------------
+	--- Molten Core ---
+	-------------------
+
+Item17073 = "YES"; --Earthshaker
+Item18822 = "YES"; --Obsidian Edged Blade
+Item17071 = "YES"; --Gutgore Ripper
+Item17074 = "YES"; --Shadowstrike
+Item18803 = "YES"; --Finkle's Lava Dredger
+Item17076 = "YES"; --Bonereaver's Edge
+Item17104 = "YES"; --Spinal Reaper
+
+	---------------------
+	--- Single Bosses ---
+	---------------------
+
+Item17067 = "YES"; --Ancient Cornerstone Grimoire
+Item18423 = "YES"; --Head of Onyxia
+Item18403 = "YES"; --Head of Onyxia
+Item18404 = "YES"; --Head of Onyxia
+Item18406 = "YES"; --Head of Onyxia
+
+Item18547 = "YES"; --Unmelting Ice Girdle
+Item20619 = "YES"; --Acid Inscribed Greaves
+Item20579 = "YES"; --Green Dragonskin Cloak
+Item20644 = "YES"; --Nightmare Engulfed Object
+Item20600 = "YES"; --Nightmare Engulfed Object
+Item20633 = "YES"; --Unnatural Leather Spaulders
+
+	--------------
+	--- 20 Man ---
+	--------------
+Item19802 = "YES"; --Heart of Hakkar
+Item19950 = "YES"; --Heart of Hakkar
+Item19949 = "YES"; --Heart of Hakkar
+Item19948 = "YES"; --Heart of Hakkar	
+	
+Item21220 = "YES"; --Head of Ossirian the Unscarred
+Item21504 = "YES"; --Head of Ossirian the Unscarred
+Item21507 = "YES"; --Head of Ossirian the Unscarred
+Item21505 = "YES"; --Head of Ossirian the Unscarred
+Item21506 = "YES"; --Head of Ossirian the Unscarred	
+	
 
 };
 
